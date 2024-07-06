507d194d85e8b6ed7d4095eae729226c5e417dbc
azcloudberg <120568586+azcloudberg@users.noreply.github.com>
2024-07-06 22:47:48 +0530

    added index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hi Welcome to this JS course");
+var id = 10;
+console.log(id);
+
+var firstName = "Jayanth"
+var lastName = "Nair"
+
+console.log("My name is",firstName, lastName)
+
+var messg = "Hi this is an alert!"
+
+// window.alert(messg);
+// window.prompt("Your Bid"100);
+
+window.confirm("Do you want to delete?")
