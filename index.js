a0fdffde78f3a6d5c448c7baf796354a8309657a
azcloudberg <120568586+azcloudberg@users.noreply.github.com>
2024-07-07 22:56:18 +0530

    Day2 Code

--- index.js
+++ index.js
@@ -1,15 +1,188 @@
-console.log("Hi Welcome to this JS course");
-var id = 10;
-console.log(id);
+// console.log("Hi Welcome to this JS course");
+// var id = 10;
+// console.log(id);
 
-var firstName = "Jayanth"
-var lastName = "Nair"
+// var firstName = "Jayanth"
+// var lastName = "Nair"
 
-console.log("My name is",firstName, lastName)
+// console.log("My name is",firstName, lastName)
 
-var messg = "Hi this is an alert!"
+// var messg = "Hi this is an alert!"
 
 // window.alert(messg);
 // window.prompt("Your Bid"100);
 
-window.confirm("Do you want to delete?")
+// window.confirm("Do you want to delete?")
+
+// Declared a age variable
+
+var age;
+
+/* This is a 
+multiline comment */
+
+// What are variables?
+
+// Login form ->  emailid , password
+
+var email = "ABC"; //global scope
+
+function demo(){
+    var firstname ="Navneet";
+    var ip = 1; //local scope
+}
+// console.log(email)
+// console.log(ip);
+
+var name = "Harish";
+
+let id = "XR0123";
+const roll = 1;
+
+
+var firstname = "Krish";
+
+
+console.log(x);
+var x = 5;
+console.log(x);
+
+var y = 10;
+var y = "Human";
+console.log(y);
+
+// console.log(z); // undefined???
+let z = 30;
+// let z = 50;
+console.log(z); //30 or 50??
+
+// roll = 2; // we can not do reassignment to a const variable
+console.log(roll);
+
+
+console.log(a); 
+var a; //global
+var a = 20; //accepted
+var a =30;
+a = 50;
+
+a = "Just a string";
+
+
+let xx = 10;
+// let xx = 20; Not possible
+xx = 40;
+xx = "Rahul";
+console.log(xx);
+
+let num = 10;
+console.log(num);
+
+// Variable Naming:
+
+let userName;
+let age20;
+let _matchScore
+let $newData
+
+// let user_name; Not allowed
+// let 20age; Not allowed
+// let match-score No special character
+// let var Reserved keywords are not allowed
+
+
+
+// Number
+let salary = 10000;
+let price = 101.99;
+
+// Strings
+
+let message = "Hello World"
+typeof message;
+
+// Boolean - It represents a logical value and can have two values: true or false.
+let isOnline = true;
+isOnline = false;
+
+// Undefined - It represents a variale that has not been assigned a value is undefined.
+let websiteData;
+console.log(websiteData);
+
+let friends = null;
+console.log(friends);
+
+
+// friends = 20;
+console.log(friends);
+
+// variables without initialization are undefined, null is explicitly assigned.
+
+// Reference Values
+
+// Objects - Objects are the collection of key value pairs, in which key is a string and each value can be any data type.
+
+let user = {
+    Name: 'Krish',
+    Age: 20,
+    phoneNumber: 8768990201,
+    email: 'abc@gmai.com'
+}
+
+console.log(user);
+// object.property
+console.log(user.Name);
+console.log(user["phoneNumber"]);
+// console.log(user.["phoneNumber"]);
+console.log(user.phoneNumber);
+
+// Arrays - Arrays are collection of similar type of data or homogenous elements.
+
+let fruits = ['apple', 'mango', 'banana','abc','bcd'];
+let number = [1,2,3,'ram',5,'mango'];
+console.log(fruits);
+console.log(number);
+
+console.log(fruits[2]);
+console.log(number[3]);
+
+console.log("Index number of Banana is:",fruits.indexOf('banana')); //Incase you want to know the index number
+
+console.log(fruits.indexOf('bcd'));
+
+fruits.push('Myfruit');
+console.log(fruits);
+fruits.pop('Myfruit');
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
